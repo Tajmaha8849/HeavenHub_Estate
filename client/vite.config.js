2110e94bd3cb88810015f8c46a3e2edaaf729fab
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2024-12-10 15:45:41 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig(({ mode }) => {
-  const config = {
-    plugins: [react()],
-  };
-
-  if (mode === 'development') {
-    config.server = {
-      proxy: {
-        '/api': {
-          target: 'https://heaven-hub-estate-api.vercel.app',
-          secure: false,
-        },
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://heaven-hub-estate-api.vercel.app',  // Ensure this is correct
+        changeOrigin: true,  // Ensure the origin is properly changed to the target
+        secure: true,  // If you're using a valid SSL certificate on the backend, set to true
       },
-    };
-  }
+    },
+  },
 
-  return config;
+  plugins: [react()],
 });
